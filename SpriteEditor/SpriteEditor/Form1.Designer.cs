31c0e5d01d1bec12e7d0562832a5dadb0faf3db6
Jon Wingrove <floatstarpx@gmail.com>
2017-05-08 16:30:23 +0100

    testing sprite editor- changed a sprite a little...

---
+++ SpriteEditor/SpriteEditor/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace SpriteEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.widthTextBox = new System.Windows.Forms.TextBox();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
+            this.SpriteToCodeButton = new System.Windows.Forms.Button();
+            this.CodeToSpriteButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(448, 419);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(466, 76);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 161);
+            this.textBox1.TabIndex = 1;
+            // 
+            // widthTextBox
+            // 
+            this.widthTextBox.Location = new System.Drawing.Point(466, 12);
+            this.widthTextBox.Name = "widthTextBox";
+            this.widthTextBox.Size = new System.Drawing.Size(69, 20);
+            this.widthTextBox.TabIndex = 2;
+            this.widthTextBox.Text = "16";
+            // 
+            // heightTextBox
+            // 
+            this.heightTextBox.Location = new System.Drawing.Point(541, 12);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(69, 20);
+            this.heightTextBox.TabIndex = 3;
+            this.heightTextBox.Text = "16";
+            // 
+            // SpriteToCodeButton
+            // 
+            this.SpriteToCodeButton.Location = new System.Drawing.Point(466, 243);
+            this.SpriteToCodeButton.Name = "SpriteToCodeButton";
+            this.SpriteToCodeButton.Size = new System.Drawing.Size(178, 23);
+            this.SpriteToCodeButton.TabIndex = 4;
+            this.SpriteToCodeButton.Text = "Sprite -> Code";
+            this.SpriteToCodeButton.UseVisualStyleBackColor = true;
+            this.SpriteToCodeButton.Click += new System.EventHandler(this.SpriteToCodeButton_Click);
+            // 
+            // CodeToSpriteButton
+            // 
+            this.CodeToSpriteButton.Location = new System.Drawing.Point(466, 272);
+            this.CodeToSpriteButton.Name = "CodeToSpriteButton";
+            this.CodeToSpriteButton.Size = new System.Drawing.Size(178, 23);
+            this.CodeToSpriteButton.TabIndex = 5;
+            this.CodeToSpriteButton.Text = "Code -> Sprite";
+            this.CodeToSpriteButton.UseVisualStyleBackColor = true;
+            this.CodeToSpriteButton.Click += new System.EventHandler(this.CodeToSpriteButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(466, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Resize";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 443);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CodeToSpriteButton);
+            this.Controls.Add(this.SpriteToCodeButton);
+            this.Controls.Add(this.heightTextBox);
+            this.Controls.Add(this.widthTextBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox widthTextBox;
+        private System.Windows.Forms.TextBox heightTextBox;
+        private System.Windows.Forms.Button SpriteToCodeButton;
+        private System.Windows.Forms.Button CodeToSpriteButton;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SpriteEditor/SpriteEditor/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpriteEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<Button> m_createdButtons = new List<Button>();
+        int[,] m_cols;
+        int m_width;
+        int m_height;
+
+        private Color[] m_colors =
+        {
+            Color.Aqua,
+            Color.White,
+            Color.Gray,
+            Color.Black
+        };
+
+        void RefreshView()
+        {
+            int idx = 0;
+            for (int xx = 0; xx < m_width; ++xx)
+            {
+                for (int yy = 0; yy < m_height; ++yy)
+                {
+                    m_createdButtons[idx].BackColor = m_colors[m_cols[xx, yy]];
+                    ++idx;
+                }
+            }
+        }
+
+        void SpriteToText()
+        {
+            string output = "";
+            for(int y = 0; y < m_height; ++y)
+            {
+                output += "\n";
+                for(int x = 0; x < m_width; x+=4)
+                {
+                    uint v = 0;
+                    v += (uint)m_cols[x+3, y];
+                    v += (uint)(m_cols[x+2, y]) << 2;
+                    v += (uint)(m_cols[x + 1, y]) << 4;
+                    v += (uint)(m_cols[x + 0, y]) << 6;
+
+                    byte r = (byte)v;
+                    string val = string.Format("0x{0:x2}", r);
+
+                    output += val;
+                    output += ", ";
+                }
+                output += "\r";
+            }
+            textBox1.Text = output;
+        }
+
+        void TextToSprite()
+        {
+            string ts = textBox1.Text;
+            string[] splits = ts.Split(",".ToCharArray());
+            int x = 0;
+            int y = 0;
+
+            foreach(string sp in splits)
+            {
+                string sps = sp.Replace(" ", "").Replace("\n", "").Replace("\r","");
+                if (sps.Length >= 2)
+                {
+                    uint v = Convert.ToUInt32(sps.Substring(2), 16);
+                    uint d = (v) & 3;
+                    uint c = (v >> 2) & 3;
+                    uint b = (v >> 4) & 3;
+                    uint a = (v >> 6) & 3;
+
+                    //Console.WriteLine(v + " " + a + " " + b + " " + c + " " + d);
+
+                    m_cols[x, y] = (int)a;
+                    x++; if (x == m_width) { x = 0; y++; }
+                    m_cols[x, y] = (int)b;
+                    x++; if (x == m_width) { x = 0; y++; }
+                    m_cols[x, y] = (int)c;
+                    x++; if (x == m_width) { x = 0; y++; }
+                    m_cols[x, y] = (int)d;
+                    x++; if (x == m_width) { x = 0; y++; }
+                    
+                }
+            }
+
+            RefreshView();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach(Button b in m_createdButtons)
+            {
+                panel1.Controls.Remove(b);
+            }
+            m_createdButtons.Clear();
+
+            m_width = System.Convert.ToInt32(widthTextBox.Text);
+            m_height = System.Convert.ToInt32(heightTextBox.Text);
+
+            m_cols = new int[m_width, m_height];
+
+            int size = (int)((float)Math.Min(panel1.Width, panel1.Height) / (float)Math.Max(m_width, m_height));
+
+            for (int xx=0;xx< m_width; ++xx)
+            {
+                for(int yy=0;yy< m_height; ++yy)
+                {
+                    Button nb = new Button();
+                    panel1.Controls.Add(nb);
+                    nb.Width = size;
+                    nb.Height = size;
+                    nb.Left = xx * size;
+                    nb.Top = yy * size;
+
+                    int tx = xx;
+                    int ty = yy;
+                    nb.Click += (s, args) =>
+                    {
+                        m_cols[tx, ty]++;
+                        if(m_cols[tx,ty] ==4)
+                        {
+                            m_cols[tx, ty] = 0;
+                        }
+                        RefreshView();
+                    };
+
+                    m_createdButtons.Add(nb);
+                }
+            }
+
+            RefreshView();
+        }
+
+        private void CodeToSpriteButton_Click(object sender, EventArgs e)
+        {
+            TextToSprite();
+        }
+
+        private void SpriteToCodeButton_Click(object sender, EventArgs e)
+        {
+            SpriteToText();
+        }
+    }
+}
